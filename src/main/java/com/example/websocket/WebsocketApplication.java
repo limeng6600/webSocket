381a406b40309b4c416ea939ea16547d7b58b041
limeng6600 <229898977@qq.com>
2018-07-24 10:36:07 +0800

    .

---
+++ src/main/java/com/example/websocket/WebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class WebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketApplication.class, args);
+	}
+}
